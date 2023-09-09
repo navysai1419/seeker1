6639a75ff6ee933adf218ea44c3308ef6f40ca65
Navya <navyadummu@gmail.com>
2023-09-09 12:56:04 +0530

    Your commit message

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+interface SideNavToggle{
+  screenWidth:number;
+  collapsed:boolean;
+  }
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'admin-panel-layout';
+  isSideNavCollapsed=false;
+  screenWidth=0;
+  sideBarOpen = true;
+
+  sideBarToggler():void {
+    this.sideBarOpen = !this.sideBarOpen;
+  }
+
+  
+  onToggleSideNav(data:SideNavToggle):void{
+    this.screenWidth=data.screenWidth;
+    this.isSideNavCollapsed=data.collapsed;
+
+
+  }
+ 
+}
---
+++ src/app/gis/gis.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+// import {} from 'googlemaps';
+
+@Component({
+  selector: 'app-gis',
+  templateUrl: './gis.component.html',
+  styleUrls: ['./gis.component.scss']
+})
+export class GisComponent {
+
+}
